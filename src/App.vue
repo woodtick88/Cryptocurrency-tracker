369df5b29957d223d241f474c8a406c9d2652d2f
woodtick88 <woodtick88@gmail.com>
2018-06-19 11:58:56 +0300

    add Cryptocurrency API

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
                     <v-toolbar color="blue" dark>
                         <v-toolbar-title>Tracker</v-toolbar-title>
                         <v-spacer></v-spacer>
-                        <v-text> USD | EURO </v-text>
+                        <span> USD | EURO </span>
                     </v-toolbar>
                     <v-list>
                         <v-list-tile avatar @click="">
@@ -29,11 +29,32 @@
 </template>
 
 <script>
+
+import axios from "axios"
+
+
 export default {
   data () {
     return {
-
+        cryptocurrencies: [],
+        errors: [],
+        images: {
+             BTC: 'https://www.cryptocompare.com/media/19633/btc.png',
+             IOT: 'https://www.cryptocompare.com/media/1383540/iota_logo.png',
+             ETH: 'https://www.cryptocompare.com/media/20646/eth_logo.png',
+             XMR: 'https://www.cryptocompare.com/media/19969/xmr.png',
+             LTC: 'https://www.cryptocompare.com/media/19782/litecoin-logo.png',
+             ZEC: 'https://www.cryptocompare.com/media/351360/zec.png',
+             ZCL: 'https://www.cryptocompare.com/media/351926/zcl.png'
+         }
     }
+  },
+  created() {
+      axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,ZEC,ZCL,IOT,LTC,XMR&tsyms=USD,EUR')
+        .then(response => {
+            console.log(response.data)
+            this.cryptocurrencies = response.data;
+        }).catch(error => this.errors.push(error));
   },
   name: 'App'
 }
